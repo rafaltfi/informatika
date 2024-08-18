6768dcf6ccbecf8397d6ed24cd5911151e60d1cc
rafaltfi <145311733+rafaltfi@users.noreply.github.com>
2024-08-18 11:50:25 +0700

    Add files via upload

---
+++ Latihan 1, S 2.py
@@ -0,0 +1,21 @@
+#penulisan statement
+print("Hello World")
+
+#penulisan string
+Judul = "besok mau jalan ke jakarta"
+print(Judul)
+
+#penulisan case
+method_name = "Maylia Rafa Niswah"
+print(method_name)
+
+#penulisan blok program
+a = 20
+b = 30
+if b > a:
+    print("b is greater than a")
+
+#penulisan komentar
+"""Menggunakan Triple Tanda Petik""" 
+
+ 
